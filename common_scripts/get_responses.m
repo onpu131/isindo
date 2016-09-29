cb3cdaeaffceedd14aa1ab9d65e9a1015c4df537
Dmitry Romanov <dimaromanov2010@gmail.com>
2016-09-29 20:28:29 +0300

    Fixed simulation params

--- common_scripts/get_responses.m
+++ common_scripts/get_responses.m
@@ -2,30 +2,34 @@ clear
 settings;
 
 %Sim settings
-simulationStep = 0.0001;
-t = 0:simulationStep:3;
 load_system('object_os.mdl');
 objOsSimParams.SolverType = 'Fixed-step';
 objOsSimParams.Solver = 'ode4';
 objOsSimParams.StartTime = '0';
-objOsSimParams.StopTime = '3';
+objOsSimParams.StopTime = num2str(params.simulationTime);
 objOsSimParams.SaveTime = 'on';
 objOsSimParams.SaveOutput = 'on';
 objOsSimParams.OutputSaveName = 'data';
 objOsSimParams.LimitDataPoints = 'off';
-objOsSimParams.FixedStep = num2str(simulationStep);
+objOsSimParams.FixedStep = num2str(params.simulationStep);
 %End of sim settings
+
+%Input signal settings
+set_param('object_os/Step', 'Time', num2str(params.tau));
+%end input settings
 
 %{
 Start simulation
 %}
-for currentAmpl = (-params.exp_count/2):1:(params.exp_count/2)
-    set_param('object_os/InputGain', 'Gain', num2str(currentAmpl*10));
+for currentAmpl = 1:1:(params.exp_count)+1
+    if currentAmpl == (params.exp_count/2 +1)
+        continue
+    end
+    set_param('object_os/InputGain', 'Gain', num2str(10*(currentAmpl-params.exp_count/2-1)));
     objectOsSim = sim('object_os', objOsSimParams);
     data = objectOsSim.get('data');
     time = objectOsSim.get('tout');
-    currentSim = currentAmpl + params.exp_count/2 + 1;
-    simulation_array(:,:,currentSim) = [time, data];
+    simulation_array(:,:,currentAmpl) = [time, data];
 end
 
 
@@ -38,6 +42,7 @@ switch(str)
     case 'y'
          countOfExp = size(simulation_array);
         hold on;
+        grid on;
         for i=1:1:countOfExp(3)
             plot(simulation_array(1:countOfExp(1),1,i), simulation_array(1:countOfExp(1),2,i))
         end
--- common_scripts/settings.m
+++ common_scripts/settings.m
@@ -2,3 +2,5 @@ params.tau = 0.01;
 params.max_ampl = 100;
 params.delta_ampl = 10;
 params.exp_count = params.max_ampl*2/params.delta_ampl;
+params.simulationTime = 2;
+params.simulationStep = 0.0001;
