03793ef7dabceb0e5174ca6233c4875ad124ed3c
Dmitry Romanov <dimaromanov2010@gmail.com>
2016-09-29 16:56:25 +0300

    Added response saving function

--- common_scripts/get_responses.m
+++ common_scripts/get_responses.m
@@ -1,6 +1,7 @@
 clear
 settings;
 
+%Sim settings
 simulationStep = 0.0001;
 t = 0:simulationStep:3;
 load_system('object_os.mdl');
@@ -13,12 +14,52 @@ objOsSimParams.SaveOutput = 'on';
 objOsSimParams.OutputSaveName = 'data';
 objOsSimParams.LimitDataPoints = 'off';
 objOsSimParams.FixedStep = num2str(simulationStep);
-objectOsSim = sim('object_os', objOsSimParams)
+%End of sim settings
 
-data = objectOsSim.get('data');
-time = objectOsSim.get('tout');
+%{
+Start simulation
+%}
+for currentAmpl = 1:1:10
+    set_param('object_os/InputGain', 'Gain', num2str(currentAmpl*10));
+    objectOsSim = sim('object_os', objOsSimParams);
+    data = objectOsSim.get('data');
+    time = objectOsSim.get('tout');
+    simulation_array(:,:,currentAmpl) = [time, data];
+end
 
+
+%{
+Plotting
+%}
+prompt = 'Display plot? y/n [n]: ';
+str = input(prompt,'s');
+switch(str)
+    case 'y'
+         countOfExp = size(simulation_array);
+        hold on;
+        for i=1:1:countOfExp(3)
+            plot(simulation_array(1:countOfExp(1),1,i), simulation_array(1:countOfExp(1),2,i))
+        end
+    otherwise
+end
+%end plotting
+
+
+%{
+Saving results
+%}
+prompt = 'Save results? y/n [n]: ';
+str = input(prompt,'s');
+switch(str)
+    case 'y'
+        [currentPath, name,ext] = fileparts(mfilename('fullpath'));
+        fileName = [currentPath,'\..\responses\'];
+        fileName = [fileName, 'object_os@impulse#'];
+        fileName = [fileName num2str(params.exp_count)];
+        fileName = [fileName, '.mat'];
+        save(fileName, 'simulation_array');
+    otherwise
+end
+%end saving results
 
 close_system('object_os', 0)
-
-plot(time, data)
